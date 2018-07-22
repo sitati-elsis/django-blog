a644b474970b8f12a989b06db6158f26627f2512
Elsis Sitati <sitatielsis@gmail.com>
2018-07-23 00:47:20 +0300

    configure environments'

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medium.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ['MEDIUM_SETTINGS'])
 
     from django.core.management import execute_from_command_line
 
---
+++ medium/prod_settings.py
@@ -0,0 +1,8 @@
+from medium.settings import *
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
