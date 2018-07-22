c5d7b8eb2d9ba211c700f33356570d6744254251
Elsis Sitati <sitatielsis@gmail.com>
2018-07-23 01:40:50 +0300

    add post model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,15 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    content =  models.TextField()
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    author = models.ForeignKey(User)
+    
+
+    def get_absolute_url(self):
+        return f"/blog/posts/{self.id}/"
